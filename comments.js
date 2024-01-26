716c1e37904075e3951f51ca18326e09038c282c
SalvatoreLa <salvatore.la.spata@gmail.com>
2024-01-26 12:35:05 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,48 @@
+// Create express web Server
+
+var express = require('express');
+var router = express.Router();
+var Comment = require('../models/Comment.js');
+
+// GET all comments
+router.get('/', function(req, res, next) {
+  Comment.find(function (err, comments) {
+    if (err) return next(err);
+    res.json(comments);
+  });
+});
+
+// GET a single comment by ID
+router.get('/:id', function(req, res, next) {
+  Comment.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// POST a new comment
+router.post('/', function(req, res, next) {
+  Comment.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// PUT update a comment
+router.put('/:id', function(req, res, next) {
+  Comment.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// DELETE a comment
+router.delete('/:id', function(req, res, next) {
+  Comment.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// Export router
+module.exports = router;
